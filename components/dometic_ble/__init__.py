e88f07e10b702d09644bf6ef32d93efed03df3d9
sjoram <121193063+sjoram@users.noreply.github.com>
2026-02-14 12:12:19 +0000

    Update __init__.py

--- components/dometic_ble/__init__.py
+++ components/dometic_ble/__init__.py
@@ -1,21 +1,31 @@
-from esphome.components import ble_client
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_MAC_ADDRESS
 
-DEPENDENCIES = ["ble_client"]
-AUTO_LOAD = ["sensor", "number", "select"]
+from esphome.components import ble_client
+from esphome.const import CONF_ID
+
+CODEOWNERS = ["@yourgithub"]
 
 dometic_ns = cg.esphome_ns.namespace("dometic_ble")
-DometicBLE = dometic_ns.class_("DometicBLE", cg.Component)
+DometicBLE = dometic_ns.class_("DometicBLE", cg.Component, ble_client.BLEClientNode)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(DometicBLE),
-    cv.Required(CONF_MAC_ADDRESS): cv.mac_address,
-}).extend(cv.COMPONENT_SCHEMA)
+CONF_BLE_CLIENT_ID = "ble_client_id"
+CONF_POLL_INTERVAL = "poll_interval"
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(DometicBLE),
+        cv.Required(CONF_BLE_CLIENT_ID): cv.use_id(ble_client.BLEClient),
+        cv.Optional(CONF_POLL_INTERVAL, default="10s"): cv.positive_time_period_milliseconds,
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    cg.add(var.set_mac(config[CONF_MAC_ADDRESS]))
+    parent = await cg.get_variable(config[CONF_BLE_CLIENT_ID])
+    cg.add(var.set_parent(parent))
+
+    cg.add(var.set_poll_interval(config[CONF_POLL_INTERVAL]))
